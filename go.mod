2efc49e913eb6b2fc6ccb6ba8aeb3b9495a678fb
Simon <simon.k.harv@gmail.com>
2022-06-16 09:02:47 +1000

    basic object and resolv addition

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/hajimehoshi/ebiten/v2 v2.3.4
+	github.com/solarlune/resolv v0.5.1
 	github.com/tinne26/etxt v0.0.1
 )
 
@@ -11,6 +12,7 @@ require (
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20220320163800-277f93cfa958 // indirect
 	github.com/gofrs/flock v0.8.1 // indirect
 	github.com/jezek/xgb v1.0.0 // indirect
+	github.com/kvartborg/vector v0.0.0-20200419093813-2cba0cabb4f0 // indirect
 	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
 	golang.org/x/image v0.0.0-20220321031419-a8550c1d254a // indirect
 	golang.org/x/mobile v0.0.0-20220518205345-8578da9835fd // indirect
--- scenes/scavengeScene.go
+++ scenes/scavengeScene.go
@@ -4,21 +4,31 @@ import (
 	"image/color"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/mharv/scrapyard-charter/entities"
+	"github.com/mharv/scrapyard-charter/globals"
+	"github.com/solarlune/resolv"
 )
 
 type ScavengeScene struct {
+	testObj   entities.GameObject
+	physSpace *resolv.Space
 }
 
 func (s *ScavengeScene) Init() {
+	s.physSpace = resolv.NewSpace(globals.ScreenWidth, globals.ScreenHeight, 16, 16)
+	s.testObj.Init("images/test.png")
 }
 
 func (s *ScavengeScene) ReadInput() {
+	s.testObj.ReadInput()
 }
 
 func (s *ScavengeScene) Update(state *GameState, deltaTime float64) error {
+	s.testObj.Update(deltaTime)
 	return nil
 }
 
 func (s *ScavengeScene) Draw(screen *ebiten.Image) {
-	screen.Fill(color.RGBA{255, 255, 0, 255})
+	screen.Fill(color.RGBA{0, 0, 0, 255})
+	s.testObj.Draw(screen)
 }
