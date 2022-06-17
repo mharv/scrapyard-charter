f9cab11b7ced8849e2402933f402ba612fbf04df
mharv <mitch.k.harvey@gmail.com>
2022-06-18 09:20:15 +1000

    add escape to main menu functionality to overworld scene

--- scenes/overworldScene.go
+++ scenes/overworldScene.go
@@ -4,18 +4,36 @@ import (
 	"image/color"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/mharv/scrapyard-charter/entities"
 )
 
 type OverworldScene struct {
+	entitiyManager entities.EntityManager
+	menuBtn        bool
 }
 
 func (o *OverworldScene) Init() {
 }
 
 func (o *OverworldScene) ReadInput() {
+	o.entitiyManager.ReadInput()
+
+	if inpututil.IsKeyJustPressed(ebiten.KeyEscape) {
+		o.menuBtn = true
+	} else {
+		o.menuBtn = false
+	}
 }
 
 func (o *OverworldScene) Update(state *GameState, deltaTime float64) error {
+	o.entitiyManager.Update(deltaTime)
+
+	if o.menuBtn {
+		t := &TitleScene{}
+		state.SceneManager.GoTo(t, transitionTime)
+	}
+
 	return nil
 }
 
